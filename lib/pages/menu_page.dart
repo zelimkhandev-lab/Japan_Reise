ebaf2a76fa56cb44a368210746651df50ed7f1b5
Leidan Zelimkhan <zelimkhandev@gmail.com>
2025-12-12 20:23:45 +0100

    Child with Nachlass

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:japan_reise/components/button.dart';
 
 class MenuPage extends StatelessWidget {
   const MenuPage({super.key});
@@ -17,6 +18,42 @@ class MenuPage extends StatelessWidget {
           padding: const EdgeInsets.only(right: 15.0),
           child: Icon(Icons.shopping_cart, color: Colors.white,),
         )],
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(25),
+            margin: EdgeInsets.symmetric(horizontal: 25),
+            decoration: BoxDecoration(
+              color: const Color(0xffffb46c),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  children: [
+                    Text(
+                      "32 % Nachlass",
+                      style: TextStyle(
+                        fontSize: 22,
+                      ),
+                    ),
+                    SizedBox(height: 15),
+                    MyButton(
+                      mytext: "Buchen", 
+                      event: () {},
+                    )
+                  ],
+                ),
+                Image.asset(
+                  "lib/images/japan5.png", 
+                  height: 135,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
