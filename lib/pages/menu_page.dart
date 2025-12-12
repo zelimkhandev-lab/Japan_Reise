4499c45305716f37b5c21c5b456f07d0a6aa3fcf
Leidan Zelimkhan <zelimkhandev@gmail.com>
2025-12-12 19:55:54 +0100

    MenuPage AppBar completed

--- lib/pages/menu_page.dart
+++ lib/pages/menu_page.dart
@@ -5,6 +5,19 @@ class MenuPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 215, 165, 187),
+      appBar: AppBar(
+        title: Text("J A P A N", style: TextStyle(color: Colors.white),),
+        centerTitle: true,
+        backgroundColor: Colors.transparent,
+        elevation: 0, //Schatten entfernung der appBar
+        leading: Icon(Icons.menu, color: Colors.white,),
+        actions: [Padding(
+          padding: const EdgeInsets.only(right: 15.0),
+          child: Icon(Icons.shopping_cart, color: Colors.white,),
+        )],
+      ),
+    );
   }
 }
